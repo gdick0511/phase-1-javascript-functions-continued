225b8f0d8e9386bdf8ea3be3a7d88a7bc3244c10
garrettdick <garrettd0511@gmail.com>
2021-09-21 12:38:10 -0500

    commit tree fca3cc9fb5d2803f169c31849d9aa56c4c23e33d

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`; 
+};
+
+const mondayWork = function (action = "go to the office") {
+    return `This Monday, I will ${action}.`;
+};
+
+function wrapAdjective(flare = "*") {
+    return function (adjective ="special") {
+        return `You are ${flare}${adjective}${flare}!`
+    }
+};
+
